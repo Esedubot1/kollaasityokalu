26f634ea4e743d19d9eb341491015b403a391d27
juhr01 <juho1.riipinen@gmail.com>
2024-02-24 01:06:49 +0200

    reunan säätö responsiiviseksi

--- src/components/Tab/TabFilters.tsx
+++ src/components/Tab/TabFilters.tsx
@@ -10,14 +10,10 @@ import { selectBorderSettings } from "@/redux/canvasSlice";
 import FilterControl from "@/components/Filter/FilterSliderInput"
 import clsx from "clsx"
 
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableContainer from '@mui/material/TableContainer';
-import TableRow from '@mui/material/TableRow';
-
 export default function TabFilters() {
   const [isMobile, setIsMobile] = useState(false)
   const [activeFilter, setActiveFilter] = useState<FilterIdType | null>(null)
+  const [borderControlOn, setBorderControlOn] = useState(false)
   const { uploadCount, maxImageUploads } = useCanvasImageData()
 
   const { setAddBorderAction, setBorderColorAction, setBorderThicknessAction } = useCanvasAction(); // Destructure the border settings actions
@@ -67,76 +63,99 @@ export default function TabFilters() {
   if (isMobile) {
     return (
       <div>
-
-        <div>
-          <input
-            type="checkbox"
-            checked={borderSettings.addBorder}
-            onChange={handleOutlineChange}
-            style={{ transform: "scale(1.5)", marginLeft: "5px", }}
-            disabled={uploadCount !== maxImageUploads}
-          />
-
-        </div>
-        <label className={uploadCount !== maxImageUploads ? "text-gray-500" : ""}> Lisää reuna</label>
-
-        {/*{borderSettings.addBorder && (
-               
-                  <div>
-  
-                    <div>
-                      <label>Reunan väri:</label>
-                      <input
-                        type="color"
-                        value={borderSettings.borderColor} // Set the value attribute to the color state
-                        onChange={(e) => handleColorChange(e.target.value)}
-                      />
-                    </div>
-  
-                  </div>
-                
-              )}*/}
-        {borderSettings.addBorder && (
-
-          <div>
-            <label>Reunan paksuus:</label>
-            <input
-              id={'borderThicknessSlider'}
-              type="range"
-              min="1"
-              max="20"
-              value={borderSettings.borderThickness} // Set the value attribute to the thickness state
-              onChange={handleThicknessChange}
-            />
-          </div>
-
-        )}
-
-
         <div className={clsx({
           "w-full": true,
           "flex flex-nowrap": isMobile,
           "px-2": !isMobile
         })}>
-          {filters.map((filter: FilterControlType, i: number) => {
-            return (
-              <FilterControl
-                key={`filter-${i}`}
-                id={filter.id}
-                min={filter.min}
-                max={filter.max}
-                step={filter.step}
-                newFilter={filter.newFilter}
-                isMobile={isMobile}
-                activeFilter={activeFilter}
-                setActiveFilter={setActiveFilter}
-              />
+          {(!borderControlOn && !activeFilter)
+            ? (
+              <button
+                onClick={() => setBorderControlOn(true)}
+                className={clsx(
+                  "cursor-pointer transition-colors",
+                  "min-w-20 h-20 mx-1 px-2",
+                  "flex flex-col items-center justify-center text-center",
+                  "rounded hover:bg-neutral-800",
+                )}
+                disabled={uploadCount !== maxImageUploads}
+              >
+                <h3 className={`w-full text-center text-sm font-medium ${uploadCount !== maxImageUploads ? "text-gray-500" : ""} `}>Reuna</h3>
+              </button>
             )
-          })}
-        </div>
+            : (
+              borderControlOn === true && (
+                <div className="w-full flex items-center">
+                  <button
+                    onClick={() => setBorderControlOn(false)}
+                    className={clsx(
+                      "cursor-pointer",
+                      "mx-1 px-1 h-20 w-16",
+                      "flex flex-col items-center justify-center",
+                      "rounded hover:bg-neutral-800",
+                    )}
+                  >
+                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 15 15"><path fill="currentColor" fillRule="evenodd" d="M8.842 3.135a.5.5 0 0 1 .023.707L5.435 7.5l3.43 3.658a.5.5 0 0 1-.73.684l-3.75-4a.5.5 0 0 1 0-.684l3.75-4a.5.5 0 0 1 .707-.023Z" clipRule="evenodd" /></svg>
+                    <span className="text-sm">Takaisin</span>
+                  </button>
+                  <div className="grow mx-3">
+                    <div className="flex flex-row">
+                      <div className="flex flex-col">
+                      <h3 className="w-1/2 text-left font-medium">Lisää reuna</h3>
+                        <input
+                          type="checkbox"
+                          checked={borderSettings.addBorder}
+                          onChange={handleOutlineChange}
+                          style={{ transform: "scale(1.5)", marginTop: '5px', marginRight: '40px'}}
+                        />
+                      </div>
+                      {borderSettings.addBorder && (
+                        <div className="grow mx-3">
+                      <div className="flex flex-row w-full items-center transition-colors rounded">
+                        <h3 className="w-1/2 text-left font-medium">Reunan paksuus</h3>
+                        <span className="w-1/2 text-right">
+                          {borderSettings.borderThickness}
+                        </span>
+                      </div>
+                      <div className="w-full flex justify-center items-center">
+                        <input
+                          id={'borderThicknessSlider'}
+                          type="range"
+                          min="1"
+                          max="20"
+                          value={borderSettings.borderThickness} // Set the value attribute to the thickness state
+                          onChange={handleThicknessChange}
+                          className="w-full my-2"
+                        />
+                      </div>
+                      </div>
+                      )}
+                    </div>
+                  </div>
+                </div>
+              )
+            )
+          }
+            {!borderControlOn && filters.map((filter: FilterControlType, i: number) => {
+              return (
+                <FilterControl
+                  key={`filter-${i}`}
+                  id={filter.id}
+                  min={filter.min}
+                  max={filter.max}
+                  step={filter.step}
+                  newFilter={filter.newFilter}
+                  isMobile={isMobile}
+                  activeFilter={activeFilter}
+                  setActiveFilter={setActiveFilter}
+                />
+              )
+            })}
+          </div>
       </div>
     )
   }
+
   return (
     <div>
       <div className="border-b border-neutral-800 py-4 px-2">
@@ -171,17 +190,17 @@ export default function TabFilters() {
               </span>
             </div>
             <div className="w-full flex justify-center items-center">
-            {(
-              <input
-                id={'borderThicknessSlider'}
-                type="range"
-                min="1"
-                max="20"
-                value={borderSettings.borderThickness} // Set the value attribute to the thickness state
-                onChange={handleThicknessChange}
-                className="w-full my-2"
-              />
-            )}
+              {(
+                <input
+                  id={'borderThicknessSlider'}
+                  type="range"
+                  min="1"
+                  max="20"
+                  value={borderSettings.borderThickness} // Set the value attribute to the thickness state
+                  onChange={handleThicknessChange}
+                  className="w-full my-2"
+                />
+              )}
             </div>
           </div>
         )}
